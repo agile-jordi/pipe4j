a2435771db32e6fc698fa781c8d56d9a16ddaa32
bernardo.bennett@gmail.com <bernardo.bennett@gmail.com@7fd628e6-261a-9944-1337-f486a516365a>
2010-12-07 00:18:40 +0000

    Renamed to Stream4j. Added LGPL header to all files.

--- Tools/src/test/java/pipe/txt/FixEolPipeTest.java
+++ Tools/src/test/java/pipe/txt/FixEolPipeTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010 Bernardo O. Bennett
+ * 
+ * This file is part of Stream4j.
+ * 
+ * Stream4j is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Stream4j is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the Lesser GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with Stream4j. If not, see <http://www.gnu.org/licenses/>.
+ */
 package pipe.txt;
 
 import junit.framework.TestCase;
