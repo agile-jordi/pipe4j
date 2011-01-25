613263717df8536c5583f7e10a6d9e43522f5111
bernardo.bennett@gmail.com <bernardo.bennett@gmail.com@7fd628e6-261a-9944-1337-f486a516365a>
2011-01-25 19:51:49 +0000

    Added unit test for standard in, out and err.

---
+++ Tools/src/test/java/pipe4j/std/StdTest.java
@@ -0,0 +1,64 @@
+package pipe4j.std;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import junit.framework.TestCase;
+import pipe4j.core.Pipeline;
+import pipe4j.pipe.std.Stderr;
+import pipe4j.pipe.std.Stdin;
+import pipe4j.pipe.std.Stdout;
+import pipe4j.pipe.string.StringIn;
+import pipe4j.pipe.string.StringOut;
+
+public class StdTest extends TestCase {
+	private static final String source = "foo\r\nbar";
+	private PrintStream prevOut;
+	private PrintStream prevErr;
+	private InputStream prevIn;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		prevOut = System.out;
+		prevErr = System.err;
+		prevIn = System.in;
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+
+		System.setOut(prevOut);
+		System.setErr(prevErr);
+		System.setIn(prevIn);
+	}
+
+	public void testStd() throws Exception {
+		ByteArrayInputStream in = new ByteArrayInputStream(source.getBytes());
+		System.setIn(in);
+		
+		StringOut stringOut = new StringOut();
+		Pipeline.run(new Stdin(), stringOut);
+		assertEquals(source, stringOut.getString());
+				
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+
+		Pipeline.run(new StringIn(source), new Stdout());
+
+		assertEquals(source, new String(out.toByteArray()));
+
+		ByteArrayOutputStream err = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(err));
+
+		Pipeline.run(new StringIn(source), new Stderr());
+
+		assertEquals(source, new String(out.toByteArray()));
+		
+		
+	}
+}
