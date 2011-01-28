67393c7c630521d3d177ad451cfd3a085acab908
bernardo.bennett@gmail.com <bernardo.bennett@gmail.com@7fd628e6-261a-9944-1337-f486a516365a>
2011-01-28 18:31:37 +0000

    Refactor: extract TeeOutputStream to new file.

---
+++ Tools/src/main/java/pipe4j/pipe/util/TeeOutputStream.java
@@ -0,0 +1,43 @@
+package pipe4j.pipe.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+class TeeOutputStream extends OutputStream {
+	private final OutputStream tee, out;
+
+	public TeeOutputStream(OutputStream chainedStream,
+			OutputStream teeStream) {
+		out = chainedStream;
+		tee = teeStream;
+	}
+
+	public void write(int c) throws IOException {
+		out.write(c);
+		tee.write(c);
+	}
+
+	@Override
+	public void write(byte[] b) throws IOException {
+		out.write(b);
+		tee.write(b);
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		out.write(b, off, len);
+		tee.write(b, off, len);
+	}
+
+	public void close() throws IOException {
+		flush();
+
+		out.close();
+		tee.close();
+	}
+
+	public void flush() throws IOException {
+		out.flush();
+		tee.flush();
+	}
+}
