ade88e4d7d4fc314249e1fa0fc5b29538c010c58
bernardo.bennett@gmail.com <bernardo.bennett@gmail.com@7fd628e6-261a-9944-1337-f486a516365a>
2011-08-05 17:03:33 +0000

    Fixed read/write wait calculation. Improved profile report.

--- Tools/src/main/java/pipe4j/core/connector/profile/DebugPipedInputStream.java
+++ Tools/src/main/java/pipe4j/core/connector/profile/DebugPipedInputStream.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010 Bernardo O. Bennett
+ * 
+ * This file is part of Pipe4j.
+ * 
+ * Pipe4j is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Pipe4j is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the Lesser GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with Pipe4j. If not, see <http://www.gnu.org/licenses/>.
+ */
 package pipe4j.core.connector.profile;
 
 import java.io.IOException;
@@ -16,27 +34,15 @@ public class DebugPipedInputStream extends PipedInputStream implements Profiled 
 		return 0;
 	}
 
+	/*
+	 * Other read methods end up calling this guy, which causes the wait in case
+	 * buffer is empty
+	 */
 	@Override
-	public synchronized int read() throws IOException {
+	public int read() throws IOException {
 		final long currentTime = System.currentTimeMillis();
 		final int rv = super.read();
-		readWaitTimeMilliseconds += System.currentTimeMillis() - currentTime;
-		return rv;
-	}
-
-	@Override
-	public synchronized int read(byte[] b) throws IOException {
-		final long currentTime = System.currentTimeMillis();
-		final int rv = super.read(b);
-		readWaitTimeMilliseconds += System.currentTimeMillis() - currentTime;
-		return rv;
-	}
-
-	@Override
-	public synchronized int read(byte[] b, int off, int len) throws IOException {
-		final long currentTime = System.currentTimeMillis();
-		final int rv = super.read(b, off, len);
-		readWaitTimeMilliseconds += System.currentTimeMillis() - currentTime;
+		readWaitTimeMilliseconds += (System.currentTimeMillis() - currentTime);
 		return rv;
 	}
 }
--- Tools/src/main/java/pipe4j/core/connector/profile/DebugPipedOutputStream.java
+++ Tools/src/main/java/pipe4j/core/connector/profile/DebugPipedOutputStream.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010 Bernardo O. Bennett
+ * 
+ * This file is part of Pipe4j.
+ * 
+ * Pipe4j is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * Pipe4j is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the Lesser GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with Pipe4j. If not, see <http://www.gnu.org/licenses/>.
+ */
 package pipe4j.core.connector.profile;
 
 import java.io.IOException;
@@ -23,14 +41,7 @@ public class DebugPipedOutputStream extends PipedOutputStream implements
 	}
 
 	@Override
-	public synchronized void write(byte[] b) throws IOException {
-		final long currentTime = System.currentTimeMillis();
-		super.write(b);
-		writeWaitTimeMilliseconds += System.currentTimeMillis() - currentTime;
-	}
-
-	@Override
-	public synchronized void write(byte[] b, int off, int len)
+	public void write(byte[] b, int off, int len)
 			throws IOException {
 		final long currentTime = System.currentTimeMillis();
 		super.write(b, off, len);
@@ -38,7 +49,7 @@ public class DebugPipedOutputStream extends PipedOutputStream implements
 	}
 
 	@Override
-	public synchronized void write(int b) throws IOException {
+	public void write(int b) throws IOException {
 		final long currentTime = System.currentTimeMillis();
 		super.write(b);
 		writeWaitTimeMilliseconds += System.currentTimeMillis() - currentTime;
